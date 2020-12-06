748fe453471f9451842d89949c598b99dabb578d
Anton <anton_voronin_2011@mail.ru>
2020-12-06 19:26:02 +0300

    js ended

--- index.js
+++ index.js
@@ -5,19 +5,38 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('submit', formSend);
     async function formSend (e) {
         e.preventDefault();
-    
         let error=formValidate(form);
-    
+        let formData=new FormData(form)
+
+        if(error===0){
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if(response.ok){
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                form.classList.remove('_sending');
+            }else{
+                alert("Error")
+                form.classList.remove('_sending');
+            }
+
+        } else alert("Поле должно быть заполненно");
     }
 
     function formValidate (form) {
         let error=0;
         let formReq=document.querySelectorAll('._req');
-    
+        const input = formReq[0]
         formRemoveError(input);
-        if(reqCheck(input)){
+        if(!reqCheck(input)){
             formAddError(input)
+            error++;
         }
+       return error;
     }
 
     function reqCheck(input){
@@ -25,10 +44,8 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     function formAddError (input) {
         input.parentElement.classList.add('_error');
-        input.classList.add('_error');
     }
     function formRemoveError (input) {
         input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
     }
 })
